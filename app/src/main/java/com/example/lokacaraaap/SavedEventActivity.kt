268d40a7fc5ec31ced061d5fc0e4a2832884e029
arrivo <daffarrivo@gmail.com>
2026-04-23 21:15:50 +0700

    feat: back button navigation notifsavedpage

--- app/src/main/java/com/example/lokacaraaap/SavedEventActivity.kt
+++ app/src/main/java/com/example/lokacaraaap/SavedEventActivity.kt
@@ -43,7 +43,7 @@ fun SavedEventsScreen(onNavigateBack: () -> Unit = {}) {
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = { }) {
+                    IconButton(onClick = {onNavigateBack}) {
                         Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Kembali", tint = BluePrimary)
                     }
                 },
